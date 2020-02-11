58443114fce507b8639dffddaedcaaf088beabff
Connor Detlefsen <connordetlefsen@gmail.com>
2020-02-11 21:31:09 +0000

    This is my first assignment

---
+++ HelloWorld.cpp
@@ -0,0 +1,9 @@
+#include <iostream> //preprocessor directive
+
+using namespace std;  //namespace=collection of classes
+
+int main(int argc, char** argv){
+  cout << "hello world" << endl;
+
+  return 0;
+}
